ae5700f39fa6736e30fdddd878280e2ed80b0aeb
rfarhanian <rf.tech@icloud.com>
2017-07-10 07:57:39 -0700

    makeFileScript.R improvement

--- makeFileScript.R
+++ makeFileScript.R
@@ -19,12 +19,14 @@ if(length(new.packages)) install.packages(new.packages)
 cat("Missing libraries installation is complete.", "\n")
 
 ## Part 1: Introduction to the problem
-## The data of Gross Domestic Product data for the 190 ranked countries coming from https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv
-## should be merged with the educational data coming from https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv
-
-# Here are the questions we can ask after the merge:
-# If we merge the data based on the country shortcode, how many of ids match?
-# If we sort the data frame in ascending order by GDP, what will be the 13th country in the resulting data frame?
+## World bank has released the information about 190 countries in two files. The first file contains the data of Gross Domestic Product data for the 190 ranked countries 
+## (coming from https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv). It has 
+## information about the countries with their short names, ranking, and Gross Domestic Product(https://en.wikipedia.org/wiki/Gross_domestic_product). 
+## We also have additional detailed data of these countries(coming from https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv). It consists  
+## of name of the countries, the income group, regional information, currency, latest population census, latest household survery, source of most recent income and expenditure data,
+## IMF data dissemination standard, latest trade data, latest water withdrawal data. These two files can help us find a good answer for the following questions: 
+## If we merge the country information by shortcode, how much additional detailed data do we have for 190 countries? 
+# Which country has the lowest 13th ranking? 
 # What are the average GDP rankings for the High income: OECD and High income nonOECD groups?
 # What does the plot of the GDP for all of the countries look like if you use ggplot2 to color your plot by Income Group.
 # If you cut the GDP ranking into 5 separate quantile groups, and make a table versus Income.Group, how many countries are Lower middle income but among the 38 nations with highest GDP?
@@ -47,22 +49,29 @@ cat("Educational data file is downloaded successfully.", "\n")
 #GDP data
 # The header data is not correct and must be cleaned up
 # Empty lines should be skipped.
-#
+# GDP values in some lines should be trimmed.
+# The lines after TUV (country ranked 190) are invalid. These lines are in three categories:
+# 1-Lines missing Gross Domestic Product value(line 197 to 220 in the original file).
+# 2-Some lines are empty(line 196, 221, 223, 237)
+# 3-Some lines comprise information about a region and not a country
 
 # Country data file
-# Country data file contains invalid country information that should be cleaned up. The invalid lines are in two categories.
-# 1-There are some lines that contain region information(Lines 55(East Asia & Pacific) to 58("Europe & Central Asia"), 113(Latin America & Caribbean), 
-#119(Latin America & Caribbean (all income levels)),  136(middle east), 139(Middle Income), 144(Middle East and North Africa), 153(North America), 
-#182(South Asia), 193-194(Sub-Saharan Africa), 228(world)
-
+# Country data file contains invalid country information that should be cleaned up. The invalid lines are in two categories: 
+# 1-The lines that contain regional information. 
+# Lines 55(East Asia & Pacific) to 58("Europe & Central Asia"), 113(Latin America & Caribbean), 
+# 119(Latin America & Caribbean (all income levels)),  136(middle east), 139(Middle Income), 144(Middle East and North Africa), 153(North America), 
+# 182(South Asia), 193-194(Sub-Saharan Africa), 228(world)
+# 2-The lines that have income groups as country short names.
 # Line 85(High income), 88(Heavily indebted poor countries)  
 # 120(Least developed countries: UN classification), 121(Low income), 124(Lower middle income), 125(Low & middle income), 160(High income: nonOECD),
 # 164(High income: OECD), 218(Upper middle income)
+
 #dat <- readLines("./datasets/countryData.csv", n = 5)
 #tt <- gsub("\\\\","\\",dat)
 #zz <- textConnection(tt)
 #myData <- read.csv(zz,header=TRUE,quote="\"")
 #close(zz)
+
 gdpData_content= read.csv(file =  "./datasets/fgdp.csv", header=TRUE, sep=",",fill = TRUE, quote = "", skipNul=TRUE)
 
 countryData_content= read.csv(file = "./datasets/countryData.csv", header=TRUE, sep=",", fill = TRUE, quote = "\"", skipNul=TRUE)
