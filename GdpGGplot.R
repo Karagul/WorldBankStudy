2d5300594804e648a22b3552a9fe8a8bea3071d6
rfarhanian <rf.tech@icloud.com>
2017-07-13 01:28:55 -0700

    GdpGGplot.R was missed in the previous commit

--- GdpGGplot.R
+++ GdpGGplot.R
@@ -1,8 +1,15 @@
-subsetData <-subset(mergeResult, Income.Group == "High income: OECD")
-data<-mergeResult
-Country<-mergeResult$Ranking
-GDP<-mergeResult$GDP
-IncomeGroup<-mergeResult$Income.Group
-gg <- ggplot(mergeResult, aes(x=Country, y=GDP,color=IncomeGroup)) + theme(axis.text.x =  element_blank(), axis.ticks = element_blank())
-gg<-gg + geom_point()
-print(gg)
+library(ggplot2)
+library(scales)
+getGdpGgPlot<-function()
+{
+  subsetData <-subset(mergeResult, Income.Group == "High income: OECD")
+  data<-mergeResult
+  Country<-mergeResult$Ranking
+  GDP<-mergeResult$GDP
+  IncomeGroup<-mergeResult$Income.Group
+  gg <- ggplot(mergeResult, aes(x=Country, y=GDP,color=IncomeGroup)) + theme(axis.text.x =  element_blank(), axis.ticks = element_blank())
+  gg<-gg + geom_point()
+return (gg)
+}
+
+print(getGdpGgPlot())
